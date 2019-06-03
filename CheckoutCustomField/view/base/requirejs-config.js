3feb6315995b48c82c5f5aa675873883aaa2d6df
Muhammad Nauman <mhmmd.nauman@gmail.com>
2019-06-03 09:30:53 +0500

    Initial version - checkout page custom field addition

---
+++ CheckoutCustomField/view/base/requirejs-config.js
@@ -0,0 +1,12 @@
+ var config = {
+ 	'config': {
+ 	    'mixins': {
+ 	       'Magento_Checkout/js/view/shipping': {
+ 	           'Nauman1_CheckoutCustomField/js/view/shipping-payment-mixin': true
+ 	       },
+ 	       'Magento_Checkout/js/view/payment': {
+ 	           'Nauman1_CheckoutCustomField/js/view/shipping-payment-mixin': true
+ 	       }
+ 	   }
+ 	}
+ }
