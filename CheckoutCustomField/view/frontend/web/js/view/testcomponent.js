c3094a7a3a5831be4797d97a654d2d8ddc79909f
Muhammad Nauman <mhmmd.nauman@gmail.com>
2019-06-13 15:04:46 +0500

    binding dob values to form in shipping address - successful

--- CheckoutCustomField/view/frontend/web/js/view/testcomponent.js
+++ CheckoutCustomField/view/frontend/web/js/view/testcomponent.js
@@ -1,7 +1,8 @@
 define([
     'uiElement',
     'ko',
-], function (Component,ko) {
+    'jquery',
+], function (Component,ko,$) {
     'use strict';
 
     var checkoutConfig = window.checkoutConfig;
@@ -25,7 +26,33 @@ define([
         },
         onSubmit1: function() {
            // alert(this.dob_month());
-           
+           alert($("#dob_month").val());
+           var zipcode= encodeURIComponent("93306");
+           var firstname= encodeURIComponent("Lucy");
+           var lastname = encodeURIComponent("Jones");
+           var address = encodeURIComponent("1012 Main St");
+           var dob = encodeURIComponent("8/2/1969");
+           var country = encodeURIComponent("");
+           var gender = encodeURIComponent("F");
+           var phone = encodeURIComponent("2082243569");
+           var param = "sid=SIDE18IH56181033201564336442857001616154"+
+                   "&zip="+zipcode+"&first="+firstname+"&last="+lastname+
+                   "&address="+address+"&dob="+dob+"&country="+country+
+                   "&gender="+gender+"&phone="+phone;
+            var url = "https://www.INTEGRITY-DIRECT.com/online/authentication_url.asp";
+            var count = param.length;
+           $.ajax({
+                    showLoader: true,
+                    url: url,
+                    data: param,
+                    type: "POST",
+                    headers: {  'Content-type': 'application/x-www-form-urlencoded',
+                                'Content-Length': count,
+                                'Access-Control-Allow-Origin':'*'
+                             }
+                }).done(function (data) {
+                    console.log(data);
+            });
            
            
         }
